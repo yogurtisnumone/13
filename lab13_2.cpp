15aeaa4c3de58538edb0f5551802e6a1871ea200
kachapat_p <kachapat_p@cmu.ac.th>
2024-01-05 13:25:58 +0700

    2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,31 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool size[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int i = 0; i< N ;i++ ){
+        cout << "|";  
+        for(int j = 0 ; j < M; j++){
+            if( size[i][j] ) cout << "*";
+            else cout << " ";
+        }
+        cout << "|";
+        cout << endl;
+        }
+    cout << "------------------------------------------------------------------------\n";
+    }
+
+
+void updateImage(bool img[][M] ,int s,int x,int y){
+    if(s == 1){
+        img[x][y] = 1;
+    }
+    else {
+        for(int i = 0;i < N ;i++){
+            for(int j = 0 ; j < M;j++){
+                if (sqrt(pow((i-x),2)+pow((j-y),2)) <= s-1) img[i][j] = 1;
+            }
+        }
+    }
+}
