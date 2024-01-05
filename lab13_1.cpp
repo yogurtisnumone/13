eea0fd3fffea0a275c1cf4df0177a8580567899c
kachapat_p <kachapat_p@cmu.ac.th>
2024-01-05 13:25:40 +0700

    1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,20 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double num[],int n,double output[]){
+    double sum = 0,product = 1.0 , sumPow2 = 0,Hsum = 0,max = num[0] ,min = num[0];
+    for(int i = 0;i < n; i++) {    
+        sum += num[i];
+        sumPow2 += num[i]*num[i];
+        product *= num[i];
+        Hsum += 1/num[i];
+        if(num[i] > max ) max = num[i];
+        else if(num[i] < min ) min = num[i];
+    }
+    output[0] = sum/n; 
+    output[1] = sqrt((sumPow2/n)-output[0]*output[0]);
+    output[2] = pow(product,(1.0/n));
+    output[3] = n/(Hsum);
+    output[4] = max;
+    output[5] = min;
+}
